d31d02d2ff0501f58ff1d8b19de0a22dff0b1359
OlgaKrzysztofik <olga.krzysztofik@gmail.com>
2022-06-28 00:12:33 +0200

    Center img's in Portfolio on mobiles

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -30,105 +30,105 @@ const Portfolio = () => {
       <div className={styles.grid}>
 
         <div className={styles.flexContainer}>
-        <figure className={styles.effectApollo}>
-          <img src={portfolioAxiom} alt="AxiomOS web"/>
-          <figcaption>
-            <p>Commercial website for the AxiomOS</p>
-          </figcaption>     
-        </figure>
-        <div className={styles.projectDescription}>
-          <h2>AxiomOS</h2>
-          <p>Commercial project. Website for a mobile operating system which is helping users stay private in a world where everyone follows you. The project includes HTTP requests (subscribing to the mailing list) and a countdown to the premiere of the new version of the system.</p>
-          {standardSetTools}
-          <div className={styles.linksContainer}>
-            <button><a href="http://web.axiomos.pl/" target="_blank" rel="noopener noreferrer">View it here</a></button>
-            <button><a href="https://github.com/pinkjagnie/axiom-os" target="_blank" rel="noopener noreferrer">View GitHub repo</a></button>
+          <figure className={styles.effectApollo}>
+            <img src={portfolioAxiom} alt="AxiomOS web"/>
+            <figcaption>
+              <p>Commercial website for the AxiomOS</p>
+            </figcaption>     
+          </figure>
+          <div className={styles.projectDescription}>
+            <h2>AxiomOS</h2>
+            <p>Commercial project. Website for a mobile operating system which is helping users stay private in a world where everyone follows you. The project includes HTTP requests (subscribing to the mailing list) and a countdown to the premiere of the new version of the system.</p>
+            {standardSetTools}
+            <div className={styles.linksContainer}>
+              <button><a href="http://web.axiomos.pl/" target="_blank" rel="noopener noreferrer">View it here</a></button>
+              <button><a href="https://github.com/pinkjagnie/axiom-os" target="_blank" rel="noopener noreferrer">View GitHub repo</a></button>
+            </div>
           </div>
-        </div>
         </div>
 
         <div className={styles.flexContainer}>
-        <figure className={styles.effectApollo}>
-          <img src={portfolioAxiom} alt="AxiomOS Hub web"/>
-          <figcaption>
-            <p>Commercial website for the AxiomOS Hub</p>
-          </figcaption>     
-        </figure>
-        <div className={styles.projectDescription}>
-          <h2>AxiomOS Hub</h2>
-          <p>Commercial project. This website is part of AxiomOS where the user can view application privacy reports and analyzes. The project includes search bar and Lorem ipsum dolor............. </p>
-          {standardSetTools}
-          <div className={styles.linksContainer}>
-            <button><a href="http://web.axiomos.pl/" target="_blank" rel="noopener noreferrer">View it here</a></button>
-            <button><a href="https://github.com/pinkjagnie/axiom-hub" target="_blank" rel="noopener noreferrer">View GitHub repo</a></button>
+          <figure className={styles.effectApollo}>
+            <img src={portfolioAxiom} alt="AxiomOS Hub web"/>
+            <figcaption>
+              <p>Commercial website for the AxiomOS Hub</p>
+            </figcaption>     
+          </figure>
+          <div className={styles.projectDescription}>
+            <h2>AxiomOS Hub</h2>
+            <p>Commercial project. This website is part of AxiomOS where the user can view application privacy reports and analyzes. The project includes search bar and Lorem ipsum dolor............. </p>
+            {standardSetTools}
+            <div className={styles.linksContainer}>
+              <button><a href="http://web.axiomos.pl/" target="_blank" rel="noopener noreferrer">View it here</a></button>
+              <button><a href="https://github.com/pinkjagnie/axiom-hub" target="_blank" rel="noopener noreferrer">View GitHub repo</a></button>
+            </div>
           </div>
-        </div>
         </div>
 
         <div className={styles.flexContainer}>
-        <figure className={styles.effectApollo}>
-          <img src={portfolioAxiom} alt="Pink Jagnie portfolio"/>
-          <figcaption>
-            <p>My portfolio</p>
-          </figcaption>     
-        </figure>
-        <div className={styles.projectDescription}>
-          <h2>Pink Jagnie portfolio</h2>
-          <p>A portfolio that is not boring. A website that, apart from my skills, also shows my personality.</p>
-          {standardSetTools}
-          <div className={styles.linksContainer}>
-            <button><a href="http://pinkjagnie.pl/" target="_blank" rel="noopener noreferrer">View it here</a></button>
-            <button><a href="https://github.com/pinkjagnie/portfolio-pink_jagnie" target="_blank" rel="noopener noreferrer">View GitHub repo</a></button>
+          <figure className={styles.effectApollo}>
+            <img src={portfolioAxiom} alt="Pink Jagnie portfolio"/>
+            <figcaption>
+              <p>My portfolio</p>
+            </figcaption>     
+          </figure>
+          <div className={styles.projectDescription}>
+            <h2>Pink Jagnie portfolio</h2>
+            <p>A portfolio that is not boring. A website that, apart from my skills, also shows my personality.</p>
+            {standardSetTools}
+            <div className={styles.linksContainer}>
+              <button><a href="http://pinkjagnie.pl/" target="_blank" rel="noopener noreferrer">View it here</a></button>
+              <button><a href="https://github.com/pinkjagnie/portfolio-pink_jagnie" target="_blank" rel="noopener noreferrer">View GitHub repo</a></button>
+            </div>
           </div>
-        </div>
         </div>
 
         <div className={styles.flexContainer}>
-        <figure className={styles.effectApollo}>
-          <img src={portfolioFlowers} alt="Flower store website"/>
-          <figcaption>
-            <p>Website of flowerstore "Twój bukiet"</p>
-          </figcaption>     
-        </figure>
-        <div className={styles.projectDescription}>
-          <h2>Flowerstore "Twój bukiet"</h2>
-          <p>The beginning of my journey. A website of fictional flowerstore built to learn CSS, using jQuery.</p>
-          <div className={styles.boxIcon}>
-            <AiFillHtml5 className={styles.icon}/>
-            <DiCss3 className={styles.icon}/>
-            <SiJquery className={styles.icon}/>
-            <MdWebStories className={styles.icon}/>
-            <AiFillApi className={styles.icon}/>
+          <figure className={styles.effectApollo}>
+            <img src={portfolioFlowers} alt="Flower store website"/>
+            <figcaption>
+              <p>Website of flowerstore "Twój bukiet"</p>
+            </figcaption>     
+          </figure>
+          <div className={styles.projectDescription}>
+            <h2>Flowerstore "Twój bukiet"</h2>
+            <p>The beginning of my journey. A website of fictional flowerstore built to learn CSS, using jQuery.</p>
+            <div className={styles.boxIcon}>
+              <AiFillHtml5 className={styles.icon}/>
+              <DiCss3 className={styles.icon}/>
+              <SiJquery className={styles.icon}/>
+              <MdWebStories className={styles.icon}/>
+              <AiFillApi className={styles.icon}/>
+            </div>
+            <div className={styles.linksContainer}>
+              <button><a href="https://twoj-bukiet.netlify.app/" target="_blank" rel="noopener noreferrer">View it here</a></button>
+              <button><a href="https://github.com/pinkjagnie/flowerstore-your-bouquet" target="_blank" rel="noopener noreferrer">View GitHub repo</a></button>
+            </div>
           </div>
-          <div className={styles.linksContainer}>
-            <button><a href="https://twoj-bukiet.netlify.app/" target="_blank" rel="noopener noreferrer">View it here</a></button>
-            <button><a href="https://github.com/pinkjagnie/flowerstore-your-bouquet" target="_blank" rel="noopener noreferrer">View GitHub repo</a></button>
-          </div>
-        </div>
         </div>
 
         <div className={styles.flexContainer}>
-        <figure className={styles.effectApollo}>
-          <img src={portfolioWitch} alt="Fortune generator"/>
-          <figcaption>
-            <p>Website where you will know your future</p>
-          </figcaption>     
-        </figure>
-        <div className={styles.projectDescription}>
-          <h2>Fortune generator</h2>
-          <p>Also the beginning of my journey. The website where the witch Alberta will predict your future. Project created to improve CSS and JavaScript.</p>
-          <div className={styles.boxIcon}>
-            <AiFillHtml5 className={styles.icon}/>
-            <DiCss3 className={styles.icon}/>
-            <SiJavascript className={styles.icon}/>
-            <MdWebStories className={styles.icon}/>
-            <AiFillApi className={styles.icon}/>
+          <figure className={styles.effectApollo}>
+            <img src={portfolioWitch} alt="Fortune generator"/>
+            <figcaption>
+              <p>Website where you will know your future</p>
+            </figcaption>     
+          </figure>
+          <div className={styles.projectDescription}>
+            <h2>Fortune generator</h2>
+            <p>Also the beginning of my journey. The website where the witch Alberta will predict your future. Project created to improve CSS and JavaScript.</p>
+            <div className={styles.boxIcon}>
+              <AiFillHtml5 className={styles.icon}/>
+              <DiCss3 className={styles.icon}/>
+              <SiJavascript className={styles.icon}/>
+              <MdWebStories className={styles.icon}/>
+              <AiFillApi className={styles.icon}/>
+            </div>
+            <div className={styles.linksContainer}>
+              <button><a href="https://omnes-generator.netlify.app/" target="_blank" rel="noopener noreferrer">View it here</a></button>
+              <button><a href="https://github.com/pinkjagnie/fortune-generator" target="_blank" rel="noopener noreferrer">View GitHub repo</a></button>
+            </div>
           </div>
-          <div className={styles.linksContainer}>
-            <button><a href="https://omnes-generator.netlify.app/" target="_blank" rel="noopener noreferrer">View it here</a></button>
-            <button><a href="https://github.com/pinkjagnie/fortune-generator" target="_blank" rel="noopener noreferrer">View GitHub repo</a></button>
-          </div>
-        </div>
         </div>
 
       </div>
